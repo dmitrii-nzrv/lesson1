874e21f035a720449416bda2028536d4c280324d
Dmitrii <dmitrii.nazarov7703@gmail.com>
2024-07-15 21:52:50 +0300

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by Dmitrii Nazarov on 15.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
